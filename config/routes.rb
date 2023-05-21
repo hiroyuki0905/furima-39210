4de26132a3e8e648911c638541e0449ca9c56bee
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-21 14:09:38 +0900

    ユーザー登録、ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+  resources :items do
+  get '/users', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
 
