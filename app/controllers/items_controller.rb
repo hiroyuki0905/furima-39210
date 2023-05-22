8e65603c6194287a28ad275ed1c3c4691f1e1d7c
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-22 22:48:10 +0900

    itemモデル・itemsコントローラーの生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,9 @@ class ItemsController < ApplicationController
   # indexアクションを定義した
   def index  
   end
+
+  def new
+    @item = Item.new
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items     
+   
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数混合で入力してください" }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、カタカナ、漢字で入力してください" }
