00f120e2510cc0b660d221e75bdbca8eb6b3e0c0
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-22 12:33:46 +0900

    テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
-  def index  # indexアクションを定義した
+  # indexアクションを定義した
+  def index  
   end
   
 end
