8a43611ac577844d528830b6457d954dc3e0bab4
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-30 15:26:14 +0900

    商品情報編集機能4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   
-  resources :items, except: [:edit] do
-    member do
-      get :edit
-      patch :update
+  resources :items do
+    resources :orders, only: [:index, :new, :create]
     end
-  end 
-end
+end 
