dce91c974608369d4fce800ad093df473927b112
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-30 16:02:50 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :check_user_ownership, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :check_user_ownership, only: [:edit, :update, :destroy]
 
   def new
     @item = Item.new
@@ -26,6 +26,12 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
   def update
     if @item.update(item_params)
