bb75930416b4cb20e59bf501eeaad2354b982363
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-05-30 12:03:33 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_user_ownership, only: [:edit, :update]
 
   def new
     @item = Item.new
@@ -23,7 +25,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_user_ownership
+    if user_signed_in? && @item.user_id != current_user.id
+      redirect_to root_path
+    elsif !user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   
-  resources :items 
+  resources :items, except: [:edit] do
+    member do
+      get :edit
+      patch :update
+    end
+  end 
 end
