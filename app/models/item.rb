bc4e6c27b4d83e436f19963f84ad768c75664ff1
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-06-05 14:35:46 +0900

    挙動確認　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,24 +5,33 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :order
 
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :delivery_day
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, exclusion: { in: [0], message: 'must be other than 0' }
-  validates :condition_id, presence: true, exclusion: { in: [0], message: 'must be other than 0' }
-  validates :delivery_fee_id, presence: true, exclusion: { in: [0], message: 'must be other than 0' }
-  validates :prefecture_id, presence: true, exclusion: { in: [0], message: 'must be other than 0' }
-  validates :delivery_day_id, presence: true, exclusion: { in: [0], message: 'must be other than 0' }
+  validates :category_id, presence: true 
+  validates :condition_id, presence: true
+
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true, unless: :was_attached?
 
+
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank"}
+  validates :delivery_fee_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"}
+
   def was_attached?
     image.attached?
   end
+  
 end
